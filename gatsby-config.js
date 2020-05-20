8e4784f18f3b07f5fd16d24216253946607d668e
flopreynat <flopreynat@protonmail.com>
2020-05-20 21:26:45 +0200

    adding missing plugin to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
         author: 'Myself (with Gatsby)'
     },
     plugins: [
+        'gatsby-plugin-react-helmet',
         'gatsby-plugin-sass',
         {
             resolve: 'gatsby-source-filesystem',
